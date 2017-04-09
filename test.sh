081d7ddb62b1968f5cc21d9c7cc7361cbb7f48ee
jzs <jzs@sketchground.dk>
2017-04-09 21:36:29 +0200

    Added golint to test script

--- test.sh
+++ test.sh
@@ -1,4 +1,8 @@
 echo 'Running linter'
 golint $(go list ./... | grep -v vendor)
+echo 'Running go vet'
+go vet $(go list ./... | grep -v vendor)
 echo 'Running unit tests'
 go test $(go list ./... | grep -v vendor)
+# echo 'Running integration tests'
+# go test -tags=integration $(go list ./... | grep -v vendor)
