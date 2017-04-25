6d8faf3ec006ba4b1681be0244be2d7ad9e0dccc
jzs <jzs@sketchground.dk>
2017-04-25 22:00:00 +0200

    Exclude dist dir from todo.sh

--- todo.sh
+++ todo.sh
@@ -1,1 +1,1 @@
-grep -rnw . --exclude 'moment.js' --exclude 'todo.sh' --exclude-dir 'vendor' --exclude-dir '.hg' -e 'TODO'
+grep -rnw . --exclude 'moment.js' --exclude 'todo.sh' --exclude-dir 'vendor' --exclude-dir '.hg' --exclude-dir 'dist' -e 'TODO'
