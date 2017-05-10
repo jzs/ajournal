e3143188063f99e163738a1a0bf89243781cbd5e
jzs <jzs@sketchground.dk>
2017-05-10 22:49:11 +0200

    Added transport tests for profile

---
+++ profile/transport_test.go
@@ -0,0 +1,64 @@
+package profile_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"bitbucket.org/sketchground/ajournal/profile"
+	"github.com/gorilla/mux"
+)
+
+func TestTransport(t *testing.T) {
+	m := mux.NewRouter()
+	pr := NewInmemRepo()
+	sr := NewInmemSubRepo()
+	ps := profile.NewService(pr, sr)
+	profile.SetupHandler(m, ps, &logger{})
+
+	posts := []struct {
+		URL      string
+		Code     int
+		Type     string
+		PostBody string
+	}{
+		{
+			URL:      "/profile",
+			Code:     http.StatusForbidden,
+			Type:     "PUT",
+			PostBody: "{}",
+		},
+		{
+			URL:  "/profile",
+			Code: http.StatusForbidden,
+			Type: "GET",
+		},
+		{
+			URL:      "/profile",
+			Code:     http.StatusForbidden,
+			Type:     "POST",
+			PostBody: "{}",
+		},
+	}
+
+	for _, p := range posts {
+		var req *http.Request
+		switch p.Type {
+		case "GET":
+			req, _ = http.NewRequest(p.Type, p.URL, nil)
+		case "POST":
+			req, _ = http.NewRequest(p.Type, p.URL, strings.NewReader(p.PostBody))
+		case "PUT":
+			req, _ = http.NewRequest(p.Type, p.URL, strings.NewReader(p.PostBody))
+		default:
+			req, _ = http.NewRequest(p.Type, p.URL, nil)
+		}
+
+		rw := httptest.NewRecorder()
+		m.ServeHTTP(rw, req)
+		if rw.Code != p.Code {
+			t.Errorf("Expected %v on url %v, got %v", p.Code, p.URL, rw.Code)
+		}
+	}
+}
