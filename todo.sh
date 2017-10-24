afba2e012a209fffec41c10e7cb3841f217ea867
Jens Zeilund <jzs@sketchground.dk>
2017-10-24 20:39:18 +0200

    Add exlude dirs to todo script

--- todo.sh
+++ todo.sh
@@ -1,1 +1,1 @@
-grep -rnw . --exclude 'moment.js' --exclude 'todo.sh' --exclude-dir 'vendor' --exclude-dir '.hg' --exclude-dir 'dist' -e 'TODO'
+grep -rnw . --exclude 'moment.js' --exclude 'todo.sh' --exclude-dir 'vendor' --exclude-dir 'tmp' --exclude-dir '.git' --exclude-dir '.hg' --exclude-dir 'dist' -e 'TODO'
